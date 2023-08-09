0c8e190673ebac9652343bdf0dd1377df392a249
ModerRAS <adqewsx@gmail.com>
2023-08-09 21:54:37 +0800

    修改`Passage`为使用`Title`

--- BannerlordTwee/Models/Passage.cs
+++ BannerlordTwee/Models/Passage.cs
@@ -4,10 +4,7 @@ using System.Text;
 
 namespace BannerlordTwee.Models {
     public class Passage {
-        public string Name { get; set; }
-        public string Position { get; set; }
-        public string Size { get; set; }
-        public List<string> Tags { get; set; }
+        public Title Title { get; set; }
         public string RawHeader { get; set; }
         public string RawContent { get; set; }
         public string Content { get; set; }
